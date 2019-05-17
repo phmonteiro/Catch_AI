ae83667df036f51a48d6e391897c6d0b51172587
Paulo Monteiro <phenriquesmonteiro@gmail.com>
2019-05-17 14:10:52 +0100

    mutation2 - insert mutation

--- src/ga/geneticOperators/Mutation2.java
+++ src/ga/geneticOperators/Mutation2.java
@@ -3,6 +3,10 @@ package ga.geneticOperators;
 import ga.IntVectorIndividual;
 import ga.Problem;
 
+import java.util.Random;
+
+import static ga.GeneticAlgorithm.random;
+
 
 public class Mutation2<I extends IntVectorIndividual, P extends Problem<I>> extends Mutation<I, P> {
 
@@ -12,13 +16,41 @@ public class Mutation2<I extends IntVectorIndividual, P extends Problem<I>> exte
 
     @Override
     public void mutate(I ind) {
-        //TODO
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        int primeiroAlelo = random.nextInt(ind.getNumGenes());
+        int segundoAlelo = 0;
+
+        do {
+
+            if (primeiroAlelo == ind.getNumGenes()){
+                segundoAlelo = random.nextInt(ind.getNumGenes()-1);
+            } else {
+                segundoAlelo = random.nextInt(ind.getNumGenes());
+            }
+
+            if (primeiroAlelo > segundoAlelo) {
+
+                int auxTrocaAlelos = primeiroAlelo;
+                primeiroAlelo = segundoAlelo;
+                segundoAlelo = auxTrocaAlelos;
+            }
+        } while (primeiroAlelo == segundoAlelo && primeiroAlelo+1 == segundoAlelo);
+
+        int seguinteAoPrimeiro = primeiroAlelo+1;
+        int auxSegundoAllello = segundoAlelo;
+
+        for (int i=segundoAlelo; i>primeiroAlelo+1; i--) {
+            ind.setGene(i, ind.getGene(i-1));
+        }
+
+        ind.setGene(seguinteAoPrimeiro, auxSegundoAllello);
+
+//        int auxGeneSeguinteAoPrimeiroAlelo = ind.getGene(segundoAlelo);
+//        ind.setGene(seguinteAoPrimeiro, ind.getGene(segundoAlelo));
+//        ind.setGene(segundoAlelo, auxGeneSeguinteAoPrimeiroAlelo);
     }
 
     @Override
     public String toString() {
         //TODO
-        throw new UnsupportedOperationException("Not Implemented Yet");
+      throw new UnsupportedOperationException("Not Implemented Yet");    }
     }
-}
